f5242d17a206f4c3810e6852616132764cb1f91a
Fernando Nogueira <fgeraldo1111@gmail.com>
2021-11-20 11:29:02 -0300

    Implementação da controller de divisores

---
+++ src/TechnicalChallenge.WebApi/Controllers/DividerController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TechnicalChallenge.Core.Dtos;
+using TechnicalChallenge.Core.Entities;
+using TechnicalChallenge.Manager.Interfaces;
+
+namespace TechnicalChallenge.WebApi.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class DividerController : ControllerBase
+    {
+        private readonly IDividerManager _dividerManager;
+        public DividerController(IDividerManager dividerManager)
+        {
+            _dividerManager = dividerManager;
+        }
+
+        [HttpGet("{number}")]
+        [ProducesResponseType(typeof(DividerResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(DividerResponse), StatusCodes.Status400BadRequest)]
+        public IActionResult Get(long number)
+        {
+            var divider = new Divider
+            {
+                Number = number
+            };
+
+            var dividerResponse = _dividerManager.GetDividersAndPrimeNumbers(divider);
+            if (!dividerResponse.IsSuccess)
+            {
+                return BadRequest(dividerResponse);
+            }
+
+            return Ok(dividerResponse);
+        }
+    }
+}
--- src/TechnicalChallenge.WebApi/Startup.cs
+++ src/TechnicalChallenge.WebApi/Startup.cs
@@ -1,15 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using TechnicalChallenge.Manager;
 
 namespace TechnicalChallenge.WebApi
 {
@@ -27,6 +22,9 @@ namespace TechnicalChallenge.WebApi
         {
 
             services.AddControllers();
+
+            services.AddManager();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "TechnicalChallenge.WebApi", Version = "v1" });
