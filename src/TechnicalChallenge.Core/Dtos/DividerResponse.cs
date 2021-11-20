4240088400631b5b5271fd2b8ddc861de2226ce3
Fernando Nogueira <fgeraldo1111@gmail.com>
2021-11-19 21:15:08 -0300

    Adicionado propriedade booleana IsSuccess e string Error

--- src/TechnicalChallenge.Core/Dtos/DividerResponse.cs
+++ src/TechnicalChallenge.Core/Dtos/DividerResponse.cs
@@ -4,6 +4,8 @@ namespace TechnicalChallenge.Core.Dtos
 {
     public class DividerResponse
     {
+        public bool IsSuccess { get; set; }
+        public string Error { get; set; }
         public List<long> Dividers { get; set; } = new List<long>();
         public List<long> PrimeNumbers { get; set; } = new List<long>();
     }
