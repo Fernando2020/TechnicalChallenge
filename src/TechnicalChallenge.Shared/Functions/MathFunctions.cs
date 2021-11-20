d7a67555863e0ed00e17cd46e333fed6da5c4694
Fernando Nogueira <fgeraldo1111@gmail.com>
2021-11-19 21:07:44 -0300

    Classe responsável pelos cálculos matemáticos

---
+++ src/TechnicalChallenge.Shared/Functions/MathFunctions.cs
@@ -0,0 +1,34 @@
+﻿namespace TechnicalChallenge.Shared.Functions
+{
+    public static class MathFunctions
+    {
+        public static bool IsDivider(long divider, long dividend)
+        {
+            if (divider % dividend == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool IsPrimeNumber(long number)
+        {
+            var count = 0;
+            for (var i = 1; i <= number; i++)
+            {
+                if (IsDivider(number, i))
+                {
+                    count++;
+                }
+            }
+
+            if (count == 2)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
