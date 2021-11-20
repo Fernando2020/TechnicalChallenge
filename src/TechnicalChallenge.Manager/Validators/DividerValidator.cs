7f42df616f68f26f4d4a86e49399b3926d937a38
Fernando Nogueira <fgeraldo1111@gmail.com>
2021-11-19 21:31:51 -0300

    Classe de validação da entidade Divider

---
+++ src/TechnicalChallenge.Manager/Validators/DividerValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using TechnicalChallenge.Core.Entities;
+
+namespace TechnicalChallenge.Manager.Validators
+{
+    public class DividerValidator : AbstractValidator<Divider>
+    {
+        public DividerValidator()
+        {
+            RuleFor(x => x.Number)
+                .NotNull().GreaterThan(0).WithMessage("Digite um número maior que 0");
+        }
+    }
+}
