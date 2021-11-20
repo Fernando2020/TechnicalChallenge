c62e50fd18f6b6f058979d194e0b3efad2472fd2
Fernando Nogueira <fgeraldo1111@gmail.com>
2021-11-20 09:39:17 -0300

    Criação de testes para os cálculos matemáticos e criação das listas de divisores e primos

---
+++ src/TechnicalChallenge.Tests/Functions/MathFunctionsTest.cs
@@ -0,0 +1,95 @@
+﻿using TechnicalChallenge.Shared.Functions;
+using Xunit;
+
+namespace TechnicalChallenge.Tests.Functions
+{
+    public class MathFunctionsTest
+    {
+        [Theory]
+        [InlineData(8, 1)]
+        [InlineData(8, 2)]
+        [InlineData(8, 4)]
+        [InlineData(8, 8)]
+        public void IsDivider_True_Theory(long divider, long dividend)
+        {
+            var exec = MathFunctions.IsDivider(divider, dividend);
+
+            Assert.True(exec);
+        }
+
+        [Theory]
+        [InlineData(8, 3)]
+        [InlineData(8, 5)]
+        [InlineData(8, 9)]
+        public void IsDivider_False_Theory(long divider, long dividend)
+        {
+            var exec = MathFunctions.IsDivider(divider, dividend);
+
+            Assert.False(exec);
+        }
+
+        [Theory]
+        [InlineData(2)]
+        [InlineData(5)]
+        [InlineData(7)]
+        public void IsPrimeNumbers_True_Theory(long number)
+        {
+            var exec = MathFunctions.IsPrimeNumber(number);
+
+            Assert.True(exec);
+        }
+
+        [Theory]
+        [InlineData(4)]
+        [InlineData(8)]
+        [InlineData(10)]
+        [InlineData(20)]
+        [InlineData(30)]
+        public void IsPrimeNumbers_False_Theory(long number)
+        {
+            var exec = MathFunctions.IsPrimeNumber(number);
+
+            Assert.False(exec);
+        }
+
+        [Fact]
+        public void IsDivider_True_Fact()
+        {
+            var number = 45;
+
+            var exec = MathFunctions.IsDivider(number, 5);
+
+            Assert.True(exec);
+        }
+
+        [Fact]
+        public void IsDivider_False_Fact()
+        {
+            var number = 45;
+
+            var exec = MathFunctions.IsDivider(number, 2);
+
+            Assert.False(exec);
+        }
+
+        [Fact]
+        public void IsPrimeNumbers_True_Fact()
+        {
+            var number = 7;
+
+            var exec = MathFunctions.IsPrimeNumber(number);
+
+            Assert.True(exec);
+        }
+
+        [Fact]
+        public void IsPrimeNumbers_False_Fact()
+        {
+            var number = 8;
+
+            var exec = MathFunctions.IsPrimeNumber(number);
+
+            Assert.False(exec);
+        }
+    }
+}
---
+++ src/TechnicalChallenge.Tests/Implementations/DividerManagerTest.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using TechnicalChallenge.Core.Entities;
+using TechnicalChallenge.Manager.Implementations;
+using Xunit;
+
+namespace TechnicalChallenge.Tests.Implementations
+{
+    public class DividerManagerTest
+    {
+        private readonly DividerManager _dividerManager;
+        public DividerManagerTest()
+        {
+            _dividerManager = new DividerManager();
+        }
+
+        [Fact]
+        public void GetDividerAndPrimeNumbers_Dividers_IsSuccess_True()
+        {
+            var divider = new Divider { Number = 8 };
+            var listDividers = new List<long> { 1, 2, 4, 8 };
+
+            var response = _dividerManager.GetDividersAndPrimeNumbers(divider);
+
+            Assert.True(response.IsSuccess);
+            Assert.Null(response.Error);
+            Assert.Equal(response.Dividers, listDividers);
+        }
+
+        [Fact]
+        public void GetDividerAndPrimeNumbers_PrimeNumbers_IsSuccess_True()
+        {
+            var divider = new Divider { Number = 8 };
+            var listPrimeNumbers = new List<long> { 2 };
+
+            var response = _dividerManager.GetDividersAndPrimeNumbers(divider);
+
+            Assert.True(response.IsSuccess);
+            Assert.Null(response.Error);
+            Assert.Equal(response.PrimeNumbers, listPrimeNumbers);
+        }
+
+        [Fact]
+        public void GetDividerAndPrimeNumbers_Dividers_IsSuccess_False()
+        {
+            var divider = new Divider { Number = 0 };
+            var listDividers = new List<long>();
+
+            var response = _dividerManager.GetDividersAndPrimeNumbers(divider);
+
+            Assert.False(response.IsSuccess);
+            Assert.NotNull(response.Error);
+            Assert.Equal(response.Dividers, listDividers);
+        }
+
+        [Fact]
+        public void GetDividerAndPrimeNumbers_PrimeNumbers_IsSuccess_False()
+        {
+            var divider = new Divider { Number = 0 };
+            var listPrimeNumbers = new List<long>();
+
+            var response = _dividerManager.GetDividersAndPrimeNumbers(divider);
+
+            Assert.False(response.IsSuccess);
+            Assert.NotNull(response.Error);
+            Assert.Equal(response.PrimeNumbers, listPrimeNumbers);
+        }
+    }
+}
